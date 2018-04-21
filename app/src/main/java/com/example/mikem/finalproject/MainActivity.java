dfe66ff33ae88ec64f50a6ec0b871328319449d0
mpmudd2 <mpmudd2@illinois.edu>
2018-04-20 22:19:41 -0500

    Media Player and Play button added

--- app/src/main/java/com/example/mikem/finalproject/MainActivity.java
+++ app/src/main/java/com/example/mikem/finalproject/MainActivity.java
@@ -2,18 +2,24 @@ package com.example.mikem.finalproject;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "Main Menu";
     private static final int READ_REQUEST_CODE = 42;
     private Uri currentAudioURI = null;
+    MediaPlayer mediaPlayer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(final View v) {
                 Log.d(TAG, "Open file button clicked");
                 startOpenFile();
+            }
+        });
+
+        final ImageButton play = findViewById(R.id.playButton);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "play button clicked");
+                if (mediaPlayer != null) {
+                    if (mediaPlayer.isPlaying()) {
+                        mediaPlayer.pause();
+                    } else {
+                        mediaPlayer.start();
+                    }
+                }
             }
         });
     }
@@ -44,8 +65,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         if (requestCode == READ_REQUEST_CODE) {
+            if (mediaPlayer != null) {
+                mediaPlayer.release();
+            }
             Log.w(TAG, "Storing this Audio URI from the upload button");
             currentAudioURI = data.getData();
+            try {
+                mediaPlayer = new MediaPlayer();
+                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer.setDataSource(getApplicationContext(), currentAudioURI);
+                mediaPlayer.prepare();
+            }
+            catch (IOException ex) {
+                Log.wtf(TAG, "file does not exist");
+            }
+
         } else {
             Log.d(TAG, "requestCode was not expected: " + requestCode);
         }
